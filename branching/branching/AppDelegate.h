56c45c0230c67f0e6aae5dee51c129a3cc16ceac
jijo <jijojohnpy@gmail.com>
2015-07-06 15:23:21 +0530

    first comit

---
+++ branching/branching/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  branching
+//
+//  Created by User on 7/6/15.
+//  Copyright (c) 2015 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ branching/branching/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  branching
+//
+//  Created by User on 7/6/15.
+//  Copyright (c) 2015 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
